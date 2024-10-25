9bb49fa736f7bc61a788cfcc24c5b5775f73128a
Pier Fumagalli <pier@usrz.com>
2024-10-25 16:20:36 +0200

    Split out sanitization code

---
+++ lib/sanitize.test.ts
@@ -0,0 +1,130 @@
+import { describe, expect, it } from 'vitest'
+
+import { sanitize as sanitizeElement } from './sanitize'
+
+function sanitize(html: string): string {
+  const element = document.createElement('div')
+  element.innerHTML = html
+
+  sanitizeElement(element)
+  return element.innerHTML
+}
+
+describe('HTML Sanitizer', () => {
+  it('should strip unwanted tags', () => {
+    expect(sanitize('<script>1</script>')).toEqual('')
+    expect(sanitize('1<script>2</script>3')).toEqual('13')
+    expect(sanitize('1<script>2<b>3</b>4</script>5')).toEqual('15')
+  })
+
+  it('should preserve content for unknown tags', () => {
+    expect(sanitize('<span>1</span>')).toEqual('1')
+    expect(sanitize('1<span>2</span>3')).toEqual('123')
+    expect(sanitize('1<span>2<b>3</b>4</span>5')).toEqual('12<b>3</b>45')
+    expect(sanitize('1<b>2<span>3</span>4</b>5')).toEqual('1<b>234</b>5')
+  })
+
+  it('should normalize nested <b> and <i> tags', () => {
+    expect(sanitize('1<b>2<b>3</b>4</b>5')).toEqual('1<b>234</b>5')
+    expect(sanitize('1<b>2<strong>3</strong>4</b>5')).toEqual('1<b>234</b>5')
+    expect(sanitize('1<strong>2<b>3</b>4</strong>5')).toEqual('1<b>234</b>5')
+
+    expect(sanitize('1<i>2<i>3</i>4</i>5')).toEqual('1<i>234</i>5')
+    expect(sanitize('1<i>2<em>3</em>4</i>5')).toEqual('1<i>234</i>5')
+    expect(sanitize('1<em>2<i>3</i>4</em>5')).toEqual('1<i>234</i>5')
+  })
+
+  it('should join contiguous <b> and <i> tags', () => {
+    expect(sanitize('1<b>2</b><b>3</b>4')).toEqual('1<b>23</b>4')
+    expect(sanitize('1<b>2</b><strong>3</strong>4')).toEqual('1<b>23</b>4')
+    expect(sanitize('1<strong>2</strong><b>3</b>4')).toEqual('1<b>23</b>4')
+
+    expect(sanitize('1<i>2</i><i>3</i>4')).toEqual('1<i>23</i>4')
+    expect(sanitize('1<i>2</i><em>3</em>4')).toEqual('1<i>23</i>4')
+    expect(sanitize('1<em>2</em><i>3</i>4')).toEqual('1<i>23</i>4')
+  })
+
+  it('should strip empty <b> and <i> tags', () => {
+    expect(sanitize('1<b></b>2')).toEqual('12')
+    expect(sanitize('1<strong></strong>2')).toEqual('12')
+
+    expect(sanitize('1<i></i>2')).toEqual('12')
+    expect(sanitize('1<em></em>2')).toEqual('12')
+  })
+
+  it('should replace <p>, <div> and <br> tags with newlines', () => {
+    expect(sanitize('1<div></div>2')).toEqual('1\n2')
+    expect(sanitize('1<div>2</div>3')).toEqual('1\n2\n3')
+
+    expect(sanitize('1<p></p>2')).toEqual('1\n2')
+    expect(sanitize('1<p>2</p>3')).toEqual('1\n2\n3')
+
+    expect(sanitize('1<br>2')).toEqual('1\n2')
+  })
+
+  it('should sanitize links', () => {
+    expect(sanitize('1<a href="foo" name="bar">2</a>3')).toEqual('1<a href="foo">2</a>3')
+    expect(sanitize('1<a href="foo">2</a><a href="bar">3</a>4')).toEqual('1<a href="foo">2</a><a href="bar">3</a>4')
+    expect(sanitize('1<a href="foo">2</a>3<a href="foo">4</a>5')).toEqual('1<a href="foo">2</a>3<a href="foo">4</a>5')
+  })
+
+  it('should preserve formatting across links', () => {
+    expect(sanitize('1<b>2</b><a href="foo"><b>3</b>4</a>5'))
+        .toEqual('1<b>2</b><a href="foo"><b>3</b>4</a>5')
+    // this shoud basically rewrite links as "top level" elements
+    expect(sanitize('1<a href="foo">2</a><b><a href="foo">3</a>4</b>5'))
+        .toEqual('1<a href="foo">2<b>3</b></a><b>4</b>5')
+  })
+
+  it('should strip empty links', () => {
+    expect(sanitize('1<a href="foo"></a>2')).toEqual('12')
+    expect(sanitize('1<a href="foo"><b></b></a>2')).toEqual('12')
+    expect(sanitize('1<a name="foo"></a>2')).toEqual('12')
+    expect(sanitize('1<a name="foo"><b></b></a>2')).toEqual('12')
+  })
+
+  it('should preserve only content with links without hrefs', () => {
+    expect(sanitize('1<a>2</a>3')).toEqual('123')
+    expect(sanitize('1<a>2<b></b>3</a>4')).toEqual('1234')
+    expect(sanitize('1<a>2<b>3</b>4</a>5')).toEqual('12<b>3</b>45')
+  })
+
+  it('should join contiguous links', () => {
+    expect(sanitize('1<a href="foo">2</a><a href="foo">3</a>4')).toEqual('1<a href="foo">23</a>4')
+  })
+
+  it('should wipe nested links', () => {
+    // this is an edge case, as the parser won't normally allow this
+    const inner = document.createElement('a')
+    inner.setAttribute('href', 'inner')
+    inner.setAttribute('name', 'remove-this')
+    inner.append('nested')
+
+    const outer = document.createElement('a')
+    outer.setAttribute('href', 'outer')
+    inner.setAttribute('name', 'remove-this')
+    outer.append('before')
+    outer.append(inner)
+    outer.append('after')
+
+    const element = document.createElement('div')
+    element.append('prefix')
+    element.append(outer)
+    element.append('suffix')
+
+    sanitizeElement(element)
+
+    expect(element.innerHTML).toEqual('prefix<a href="outer">beforenestedafter</a>suffix')
+  })
+
+  it('should strp empty or invalid mentions', () => {
+    expect(sanitize('1<mention></mention>2')).toEqual('12')
+    expect(sanitize('1<mention ref="2"></mention>3')).toEqual('13')
+    expect(sanitize('1<mention>2</mention>3')).toEqual('13')
+  })
+
+  it('should sanitize mentions', () => {
+    expect(sanitize('1<mention ref="2" foo="bar">3</mention>4'))
+        .toEqual('1<mention contenteditable="false" ref="2">3</mention>4')
+  })
+})
---
+++ lib/sanitize.ts
@@ -0,0 +1,223 @@
+import { containsRange, getOffsetsRange, getRangeOffsets, rangeFromContents } from './range'
+
+import type { Offsets } from './range'
+
+/** Tags that will be stripped from output */
+const STRIP_TAGS = new Set([
+  'button',
+  'embed',
+  'form',
+  'iframe',
+  'input',
+  'object',
+  'script',
+  'select',
+  'style',
+  'textarea',
+])
+
+/**
+ * Wrap a document fragment in a tag unless `noWrap` is `true`
+ *
+ * @param fragment The fragment to wrap
+ * @param tagName The tag to wrap the fragment in
+ * @param noWrap If `true`, the fragment will not be wrapped
+ * @param attribues Additional attributes to add to the tag
+ * @returns The wrapped fragment or the fragment itself
+ */
+function maybeWrap(
+    fragment: DocumentFragment,
+    tagName: string,
+    noWrap: boolean = false,
+): DocumentFragment | Element {
+  if (noWrap) return fragment
+
+  const tag = document.createElement(tagName)
+  tag.append(fragment)
+  return tag
+}
+
+/**
+ * Sanitize a element's content for styles (bold, italic, ...)
+ *
+ * The `state` object is used to track the current state of the sanitizer: if
+ * `bold` is `true`, then all nested `<b>` and `<strong>` tags will be stripped,
+ * the same works for `italic` with `<i>` and `<em>`.
+ *
+ * @param fragment The fragment to sanitize
+ * @param state The current state of the sanitizer
+ * @returns The sanitized fragment
+ */
+function sanitizeStyles(
+    source: Element,
+    state: {
+      bold?: boolean,
+      italic?: boolean,
+    } = {},
+): DocumentFragment {
+  const result = document.createDocumentFragment()
+
+  for (const node of source.childNodes) {
+    // Text nodes are cloned if they have contents
+    if (node.nodeType === Node.TEXT_NODE) {
+      if ((node as Text).nodeValue) result.append(node.cloneNode())
+
+      // Elements are processed recursively
+    } else if (node.nodeType === Node.ELEMENT_NODE) {
+      const element = node as Element
+      const tagName = element.tagName.toLowerCase()
+
+      // Strip unwanted tags
+      if (STRIP_TAGS.has(tagName)) continue
+
+      const previous = result.lastChild?.nodeType === Node.ELEMENT_NODE ? {
+        element: result.lastChild as Element,
+        tagName: (result.lastChild as Element).tagName.toLowerCase(),
+      } : null
+
+      const child = document.createDocumentFragment()
+
+      // Bold / strong
+      if ((tagName === 'b') || (tagName === 'strong')) {
+        const fragment = sanitizeStyles(element, { ...state, bold: true })
+        if (! fragment.hasChildNodes()) continue
+
+        if (previous?.tagName === 'b') previous.element.append(fragment)
+        else child.append(maybeWrap(fragment, 'b', state.bold))
+
+        // Italic / emphasis
+      } else if ((tagName === 'i') || (tagName === 'em')) {
+        const fragment = sanitizeStyles(element, { ...state, italic: true })
+        if (! fragment.hasChildNodes()) continue
+
+        if (previous?.tagName === 'i') previous.element.append(fragment)
+        else child.append(maybeWrap(fragment, 'i', state.italic))
+
+        // Paragraphs, divs, line breaks
+      } else if ((tagName === 'br') || (tagName === 'div') || (tagName === 'p')) {
+        child.append('\n')
+        const fragment = sanitizeStyles(element, state)
+        if (fragment.hasChildNodes()) {
+          child.append(fragment)
+          child.append('\n')
+        }
+
+      // Mention is a bit of a a special case (our custom element)
+      } else if (tagName === 'mention') {
+        const ref = element.getAttribute('ref')
+        const value = element.textContent
+
+        if (!(value && ref)) continue // empty mentions are stripped
+
+        const mention = document.createElement('mention')
+        mention.setAttribute('contenteditable', 'false')
+        mention.setAttribute('ref', ref)
+        mention.append(value)
+
+        child.append(mention)
+      } else {
+        child.append(sanitizeStyles(element, state))
+      }
+
+      if (child.hasChildNodes()) result.append(child)
+    }
+  }
+
+  return result
+}
+
+/**
+ * Find the various links in an element, and return their offsets and hrefs.
+ *
+ * @param element The element for which links will be sanitized
+ * @returns An array of offsets and related hrefs of all links
+ */
+function sanitizeLinks(element: HTMLElement): (Offsets & { href: string })[] {
+  // An iterator over all the <a> elements
+  const iterator = document.createNodeIterator(element, NodeFilter.SHOW_ELEMENT, (node) => {
+    return node.nodeName.toLowerCase() === 'a' ?
+      NodeFilter.FILTER_ACCEPT :
+      NodeFilter.FILTER_SKIP
+  })
+
+  // Ranges for all our <a> elements
+  const ranges = new Set<Range & { href: string }>()
+  for (let node = iterator.nextNode(); node; node = iterator.nextNode()) {
+    const href = (node as HTMLElement).getAttribute('href')
+    if (href) ranges.add(Object.assign(rangeFromContents(node), { href }))
+  }
+
+  // Filter out any ranges that is contained within another range
+  ranges.forEach((outerRange) => {
+    ranges.forEach((innerRange) => {
+      // Here `containsRange` returns `inner` if its the same as `outer`
+      const range = containsRange(outerRange, innerRange)
+      if (range && (range !== outerRange)) ranges.delete(innerRange)
+    })
+  })
+
+  // Convert our ranges to offsets
+  const offsets = [ ...ranges ]
+      // Compute offsets for all the ranges
+      .map((range) => {
+        const offsets = getRangeOffsets(element, range)
+        return { ...offsets, href: range.href }
+      })
+      // See if we need to merge consecutive <a> with the same href
+      .filter((current, i, offsets) => {
+        const previous = offsets[i - 1]
+
+        // If it ends where this starts, and the href is the same, merge
+        if ((previous?.end === current.start) && (previous.href === current.href)) {
+          previous.end = current.end
+          return false
+        }
+
+        // Also remove any empty links
+        return current.start !== current.end
+      })
+
+  // All done, return anything we found
+  return offsets
+}
+
+/**
+ * Strip all empty elements out from our tree.
+ *
+ * @param element The element to sanitize
+ */
+function sanitizeEmpty(element: HTMLElement): void {
+  element.normalize()
+
+  const iterator = document.createNodeIterator(element, NodeFilter.SHOW_ELEMENT)
+  for (let node = iterator.nextNode(); node; node = iterator.nextNode()) {
+    if (node.hasChildNodes()) continue
+    node.parentNode?.removeChild(node)
+  }
+}
+
+export function sanitize(element: HTMLElement): void {
+  // Cleanup before we start
+  element.normalize()
+
+  // Save any and all links
+  const offsets = sanitizeLinks(element)
+
+  // Sanitize the content and replace our contents
+  const sanitized = sanitizeStyles(element)
+  element.replaceChildren(sanitized)
+
+  // Restore the links
+  offsets.forEach((offsets) => {
+    const range = getOffsetsRange(element, offsets)
+    const wrapped = range.extractContents()
+    const link = document.createElement('a')
+    link.setAttribute('href', offsets.href)
+    link.append(wrapped)
+    range.insertNode(link)
+  })
+
+  // Cleanup any empty elements which might have been introduced
+  // when restoring the links above
+  sanitizeEmpty(element)
+}
