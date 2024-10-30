4e988f3084a11b13e2a07756cbacf6339251389e
Pier Fumagalli <pier@usrz.com>
2024-10-30 17:34:08 +0100

    Demo build

--- vite.config.ts
+++ vite.config.ts
@@ -2,42 +2,45 @@ import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
 import { defineConfig } from 'vitest/config'
 
-// See: https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    minify: true,
-    sourcemap: true,
-    lib: {
-      entry: 'lib/index.ts',
-      name: 'JuitTinyEdit',
-      fileName: 'index',
-    },
-    rollupOptions: {
-      external: [ 'vue' ],
-      output: {
-        globals: { vue: 'Vue' },
+export default defineConfig((env) => {
+  const demo = env.mode === 'demo'
+  return defineConfig({
+    build: {
+      minify: true,
+      sourcemap: true,
+      outDir: demo ? 'demo' : 'dist',
+      lib: demo ? undefined :{
+        entry: 'lib/index.ts',
+        name: 'JuitTinyEdit',
+        fileName: 'index',
+      },
+      rollupOptions: {
+        external: [ 'vue' ],
+        output: {
+          globals: { vue: 'Vue' },
+        },
       },
     },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    include: [ 'lib/**/*.test.ts' ],
-    coverage: {
-      provider: 'v8',
-      reporter: [ 'text', 'html' ],
-      reportsDirectory: 'coverage',
-      cleanOnRerun: true,
-      include: [ 'lib' ],
-      exclude: [
-        'lib/**/*.test.ts',
-        'lib/editor.vue', // we always get 100% coverage???
-      ],
-      reportOnFailure: true,
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      include: [ 'lib/**/*.test.ts' ],
+      coverage: {
+        provider: 'v8',
+        reporter: [ 'text', 'html' ],
+        reportsDirectory: 'coverage',
+        cleanOnRerun: true,
+        include: [ 'lib' ],
+        exclude: [
+          'lib/**/*.test.ts',
+          'lib/editor.vue', // we always get 100% coverage???
+        ],
+        reportOnFailure: true,
+      },
     },
-  },
-  plugins: [ vue(), dts({
-    tsconfigPath: './tsconfig.app.json',
-    rollupTypes: true,
-  }) ],
+    plugins: [ vue(), dts({
+      tsconfigPath: './tsconfig.app.json',
+      rollupTypes: true,
+    }) ],
+  })
 })
